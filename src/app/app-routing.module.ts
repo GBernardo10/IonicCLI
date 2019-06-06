0365ce3b7353e407c496a6c173fc73efa68060d0
GBernardo10 <g_bernardo10@hotmail.com>
2019-06-06 12:01:01 -0300

    pagina redireciona para os componentes do menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './pages/menu/menu.module#MenuPageModule' },
   { path: 'list-user', loadChildren: './list-user/list-user.module#ListUserPageModule' },
   { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
   { path: 'main', loadChildren: './pages/main/main.module#MainPageModule' },
